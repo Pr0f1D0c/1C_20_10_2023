23b19a3461ce0e42ec8ea420e29065efccb47f0a
= <starokozhko.ig>
2023-10-20 23:38:17 +0300

    Попытка сделать линии на карте

--- src/CharacterMap.py
+++ src/CharacterMap.py
@@ -1,4 +1,6 @@
 import folium
+import pandas as pd
+
 from DatabaseManager import DatabaseManager
 
 
@@ -7,24 +9,30 @@ class CharacterMap:
         self.db_manager = db_manager
 
     def generate_map(self, map_file_path):
-        # Создаем карту с начальными координатами
-        m = folium.Map(location=[0, 0], zoom_start=1)
+        m = folium.Map(location=[0, 0], zoom_start=3)
 
-        # Получаем актуальных персонажей с их геометками
         latest_characters = self.db_manager.get_characters_with_latest_locations()
 
-        # Перебираем актуальных персонажей и добавляем метки на карту
         for char_id, character in latest_characters.items():
-            if character.latitude and character.longitude:
-                lat, lon = float(character.latitude), float(character.longitude)
-                folium.Marker([lat, lon], popup=character.name).add_to(m)
+            location = self.db_manager.get_locations_by_character(character)
+            if not location.empty:
+                latitudes = location['Latitude']
+                longitudes = location['Longitude']
+                timestemps = location['Timestamp']
+                df = pd.DataFrame({'lat': latitudes, 'long': longitudes, 'time': timestemps})
+                print(character.name, df, sep='\n')
+                df.dropna(how='any', inplace=True)
+                if len(df) > 1:
+                    sorted_df = df.sort_values(by='time')
+                    folium.PolyLine(list(zip(sorted_df['lat'], sorted_df['long'])), color="blue", weight=2.5, opacity=1).add_to(m)
+                    for lat, lon in zip(sorted_df['lat'], sorted_df['long']):
+                        folium.Marker([lat, lon], popup=character.name).add_to(m)
 
-        # Сохраняем карту в HTML-файл
         m.save(map_file_path)
 
 
 # Пример использования:
 if __name__ == '__main__':
-    db_manager = DatabaseManager('../data/common_db.csv', '../data/characters.csv')
+    db_manager = DatabaseManager('../data/Ф/common_db.csv', '../data/characters.csv')
     character_map = CharacterMap(db_manager)
     character_map.generate_map('character_map.html')
